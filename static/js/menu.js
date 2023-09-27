b50df8095226676d75e92ace36dde46a34fdcd5f
titfora <titouan821@gmail.com>
2023-09-27 12:43:53 +0200

    many fix

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,3 +1,8 @@
+const primaryCloseWidth = "0px";
+const primaryOpenWidth = "240px";
+const primaryLargeWidth = "460px";
+const collapsibleTransitionTime = 300;
+
 parent.window.onload = function () {    // wait for parent to be fully loaded
 
 /* IFRAME RESIZE */
@@ -6,12 +11,13 @@ const menu = parent.document.getElementsByClassName("menu-container")[0];
 const menuButton = document.getElementById('collapsible-menu');
 const primaryMenu = document.getElementById('menu-content');
 
-function resizeIframe(width) {
-    menu.style.width = width + 'px';
+function resizeIframe() {
+    menu.style.width = Math.max(menuButton.offsetWidth, parseInt(primaryMenu.style.width)) + 'px';
     menu.style.height = menuButton.offsetHeight + primaryMenu.offsetHeight + 'px';
 }
 
-resizeIframe(menuButton.offsetWidth);
+primaryMenu.style.width = primaryCloseWidth;    // init to avoid an empty string error
+resizeIframe();
 
 
 /* COLLAPSIBLES */
@@ -21,38 +27,41 @@ const fusexContent = document.getElementById('fusex-content');
 
 function closeAllwCheck(e) {
     if (
-        e.relatedTarget === null ||
-        e.relatedTarget !== primaryMenu &&
-        e.relatedTarget !== fusexContent &&
-        e.relatedTarget.parentElement !== primaryMenu &&
-        e.relatedTarget.parentElement !== fusexContent &&
+        (
+            e.relatedTarget === null ||
+            e.relatedTarget !== primaryMenu &&
+            e.relatedTarget !== fusexContent &&
+            e.relatedTarget.parentElement !== primaryMenu &&
+            e.relatedTarget.parentElement !== fusexContent
+        ) &&
         !menuButton.classList.contains('clicked')
     ) {
+        primaryMenu.style.width = primaryCloseWidth;
+
         primaryMenu.classList.remove('show');
         menuButton.classList.remove('show');
         fusexContent.classList.remove('show');
-        primaryMenu.style.width = "250px";
 
-        resizeIframe(menuButton.offsetWidth);
+        setTimeout(resizeIframe, collapsibleTransitionTime);
     }
 }
 
-function closeSecondarieswCheck(e, collapsible, button) {
+function closeSecondarieswCheck(e, button) {
     if (
         e.relatedTarget === null ||
         e.relatedTarget != fusexContent &&
         e.relatedTarget.parentElement !== fusexContent &&
         !button.classList.contains('clicked')
     ) {
-        closeSecondaries(primaryMenu.offsetWidth);
+        closeSecondaries();
     }
 }
 
 function closeSecondaries() {
-    fusexContent.classList.remove('show');
-    primaryMenu.style.width = "250px";
+    primaryMenu.style.width = primaryOpenWidth;
 
-    resizeIframe(250);
+    fusexContent.classList.remove('show');
+
 }
 
 
@@ -72,55 +81,88 @@ stopPropagation(fusexContent);
 
 /* MAIN */
 
-menuButton.addEventListener('click', function() {
-    menuButton.classList.toggle('clicked');
-});
+if (Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1) {
+    /* PHONE */
 
-menuButton.addEventListener("mouseover", function () {
-    primaryMenu.classList.add('show');
-    menuButton.classList.add('show');
-
-    resizeIframe(250);
-});
-
-menuButton.addEventListener("mouseout", function (e) {
-    closeAllwCheck(e);
-});
-
-primaryMenu.addEventListener("mouseout", function (e) {
-    closeAllwCheck(e);
-});
-
-
-/* FUSEX */
-
-fusexButton.addEventListener('click', function() {
-    fusexButton.classList.toggle('clicked');
-});
-
-fusexButton.addEventListener("mouseover", function () {
-    fusexContent.classList.add('show');
-    primaryMenu.style.width = "480px";
-
-    resizeIframe(480);
-});
-
-fusexButton.addEventListener("mouseout", function (e) {
-    closeAllwCheck(e);
-});
-
-fusexContent.addEventListener("mouseout", function (e) {
-    closeAllwCheck(e);
-    closeSecondarieswCheck(e, fusexContent, fusexButton);
-});
-
-
-/* OTHERS */
-
-for (const link of primaryMenu.getElementsByTagName("a")) {
-    link.addEventListener("mouseover", function (e) {
-        closeSecondaries();
+    menuButton.addEventListener('click', function() {
+        primaryMenu.style.width = primaryOpenWidth;
+        menuButton.classList.toggle('show');
+        primaryMenu.classList.toggle('show');
+        resizeIframe();
     });
+
+
+    /* FUSEX */
+
+    fusexButton.addEventListener("click", function () {
+        primaryMenu.style.width = primaryLargeWidth;
+
+        fusexContent.classList.add('show');
+
+        resizeIframe();
+    });
+
+
+    /* OTHERS */
+
+    for (const link of primaryMenu.getElementsByTagName("a")) {
+        link.addEventListener("click", function (e) {
+            closeSecondaries();
+        });
+    }
+
+} else {
+    /* COMPUTER */
+
+    menuButton.addEventListener('click', function() {
+        menuButton.classList.toggle('clicked');
+    });
+
+    menuButton.addEventListener("mouseover", function () {
+        primaryMenu.style.width = primaryOpenWidth;
+
+        primaryMenu.classList.add('show');
+        menuButton.classList.add('show');
+
+        resizeIframe();
+    });
+
+    menuButton.addEventListener("mouseout", function (e) {
+        closeAllwCheck(e);
+    });
+
+    primaryMenu.addEventListener("mouseout", function (e) {
+        closeAllwCheck(e);
+    });
+
+
+    /* FUSEX */
+
+    fusexButton.addEventListener("mouseover", function () {
+        primaryMenu.style.width = primaryLargeWidth;
+
+        fusexContent.classList.add('show');
+
+        resizeIframe();
+    });
+
+    fusexButton.addEventListener("mouseout", function (e) {
+        closeAllwCheck(e);
+    });
+
+    fusexContent.addEventListener("mouseout", function (e) {
+        closeAllwCheck(e);
+        closeSecondarieswCheck(e, fusexButton);
+    });
+
+
+    /* OTHERS */
+
+    for (const link of primaryMenu.getElementsByTagName("a")) {
+        link.addEventListener("mouseover", function (e) {
+            closeSecondaries();
+        });
+    }
 }
 
 }
