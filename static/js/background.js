c31ca7dc31b17d910cf144818dc2fc635aaa7aed
titfora <titouan821@gmail.com>
2023-10-13 23:51:57 +0200

    hello page optimization

--- static/js/background.js
+++ static/js/background.js
@@ -4,225 +4,122 @@ window.onbeforeunload = function () {
 
 /* BACKGROUND ASSO HELLO */
 
-const STAR_COLOR = '#fff';
-const STAR_SIZE = 3;
-const STAR_MIN_SCALE = 0.2;
-const OVERFLOW_THRESHOLD = 50;
-const STAR_COUNT = ( window.innerWidth + window.innerHeight ) / 8;
+const STAR_SIZE = 10;  // px
+const our_speed = 0.3; // unit distance per seconds
+const stars_container = document.getElementById('stars-container');
 
-const canvas = document.getElementById('stars-container');
-const context = canvas.getContext('2d');
-
-let scale = 1, // device pixel ratio
-    width = 1,
-    height = 1;
-
-let stars = [];
-
-let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: Math.pow((1.7 / (window.innerWidth + window.innerHeight)), 1.02) };
-
-function generate() {
-  for( let i = 0; i < STAR_COUNT; i++ ) {
-       stars.push({
-           x: 0,
-           y: 0,
-           z: Math.random() * ( 1 - STAR_MIN_SCALE ) + STAR_MIN_SCALE
-       });
-  }
+function random(a, b) {
+  return Math.random() * (b - a) + a;
 }
 
-function placeStar( star ) {
-   star.x = Math.random() * width;
-   star.y = Math.random() * height;
-}
+function randomizeStar(star, spawnFarAway) {
+  star.style.setProperty("--size", STAR_SIZE);
+  star.style.setProperty("--our-speed", our_speed);
 
-function recycleStar( star ) {
+  let initX = random(0, window.innerWidth);
+  while (initX === 0.5 * window.innerWidth) initX = random(0, window.innerWidth);
+  star.style.setProperty("--initX", initX + "px");
 
- let direction = 'z';
+  let initY = random(0, window.innerHeight);
+  while (initY === 0.5 * window.innerHeight) initY = random(0, window.innerHeight);
+  star.style.setProperty("--initY", initY + "px");
 
- let vx = Math.abs( velocity.x ),
-     vy = Math.abs( velocity.y );
-
- if( vx > 1 || vy > 1 ) {
-   let axis;
-
-   if( vx > vy ) {
-     axis = Math.random() < vx / ( vx + vy ) ? 'h' : 'v';
-   }
-   else {
-     axis = Math.random() < vy / ( vx + vy ) ? 'v' : 'h';
-   }
-
-   if( axis === 'h' ) {
-     direction = velocity.x > 0 ? 'l' : 'r';
-   }
-   else {
-     direction = velocity.y > 0 ? 't' : 'b';
-   }
- }
- 
- star.z = STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE );
-
- if( direction === 'z' ) {
-   star.z = 0.1;
-   star.x = Math.random() * width;
-   star.y = Math.random() * height;
- }
- else if( direction === 'l' ) {
-   star.x = -OVERFLOW_THRESHOLD;
-   star.y = height * Math.random();
- }
- else if( direction === 'r' ) {
-   star.x = width + OVERFLOW_THRESHOLD;
-   star.y = height * Math.random();
- }
- else if( direction === 't' ) {
-   star.x = width * Math.random();
-   star.y = -OVERFLOW_THRESHOLD;
- }
- else if( direction === 'b' ) {
-   star.x = width * Math.random();
-   star.y = height + OVERFLOW_THRESHOLD;
- }
-
-}
-
-function resize() {
-
-  const newscale = window.devicePixelRatio || 1;
-  const newwidth = window.innerWidth * newscale;
-  const newheight = window.innerHeight * newscale;
-
-  if (Math.abs(newwidth - width) / width > 0.2 || Math.abs(newheight - height) / height > 0.2) {
-    scale = newscale;
-    width = newwidth;
-    height = newheight;
-
-    canvas.width = width;
-    canvas.height = height;
-
-    stars.forEach( placeStar );
-  }
-}
-
-function step() {
-
- context.clearRect( 0, 0, width, height );
-
- update();
- render();
-
- requestAnimationFrame( step );
-
-}
-
-function update() {
-
- velocity.tx *= 0.96;
- velocity.ty *= 0.96;
-
- velocity.x += ( velocity.tx - velocity.x ) * 0.8;
- velocity.y += ( velocity.ty - velocity.y ) * 0.8;
-
- stars.forEach( ( star ) => {
-
-   star.x += velocity.x * star.z;
-   star.y += velocity.y * star.z;
-
-   star.x += ( star.x - width/2 ) * velocity.z * star.z;
-   star.y += ( star.y - height/2 ) * velocity.z * star.z;
-   star.z += velocity.z;
- 
-   // recycle when out of bounds
-   if( star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD ) {
-     recycleStar( star );
-   }
-
- } );
-
-}
-
-function render() {
-
- stars.forEach( ( star ) => {
-
-   context.beginPath();
-   context.lineCap = 'round';
-   context.lineWidth = STAR_SIZE * star.z * scale;
-   context.globalAlpha = 0.5 + 0.5*Math.random();
-   context.strokeStyle = STAR_COLOR;
-
-   context.beginPath();
-   context.moveTo( star.x, star.y );
-
-   var tailX = velocity.x * 2,
-       tailY = velocity.y * 2;
-
-   // stroke() wont work on an invisible line
-   if( Math.abs( tailX ) < 0.1 ) tailX = 0.5;
-   if( Math.abs( tailY ) < 0.1 ) tailY = 0.5;
-
-   context.lineTo( star.x + tailX, star.y + tailY );
-
-   context.stroke();
-
- } );
-
-}
-
-let previousScroll = window.scrollY;
-
-function scroll() {
-   velocity.ty += ( (previousScroll - window.scrollY) / window.innerHeight * 20.0 );
-   previousScroll = window.scrollY;
-}
-
-generate();
-resize();
-step();
-
-window.addEventListener('resize', resize);
-window.addEventListener('scroll', scroll);
-
-/* SCROLL TO ANCHOR */
-
-const asso_hello = document.getElementById("asso-hello");
-let hasScroll = false;
-
-window.addEventListener('scroll', () => {
-    if (asso_hello !== null && asso_hello.getBoundingClientRect().top <= 0 && !hasScroll) {
-      document.getElementById('asso-scrolldown').click();
-      hasScroll = true;
+  let transX_sign, transY_sign, initX_reduced, initY_reduced;
+  if (initX < 0.5 * window.innerWidth) {
+    if (initY < 0.5 * window.innerHeight) {
+      transX_sign = -1;
+      transY_sign = -1;
+      initX_reduced = initX;
+      initY_reduced = initY;
+    } else {
+      transX_sign = -1;
+      transY_sign = 1;
+      initX_reduced = initX;
+      initY_reduced = window.innerHeight - initY;
     }
-});
+  } else {
+    if (initY < 0.5 * window.innerHeight) {
+      transX_sign = 1;
+      transY_sign = -1;
+      initX_reduced = window.innerWidth - initX;
+      initY_reduced = initY;
+    } else {
+      transX_sign = 1;
+      transY_sign = 1;
+      initX_reduced = window.innerWidth - initX;
+      initY_reduced = window.innerHeight - initY;
+    }
+  }
+  const translateX = transX_sign * (Math.min(initY_reduced * (0.5 * window.innerWidth - initX_reduced) / (0.5 * window.innerHeight - initY_reduced), initX_reduced) + STAR_SIZE);
+  const translateY = transY_sign * (Math.min(initX_reduced * (0.5 * window.innerHeight - initY_reduced) / (0.5 * window.innerWidth - initX_reduced), initY_reduced) + STAR_SIZE);
+  const trajectory_distance = Math.sqrt(Math.pow(translateX, 2) + Math.pow(translateY, 2));
+  star.style.setProperty("--translate", trajectory_distance);
+
+  const angle = Math.atan2(translateY, translateX);
+  star.style.setProperty("--angle", angle);
+
+  let distance;
+  if (spawnFarAway) {
+    distance = 10;
+  } else {
+    distance = random(1, 10);
+  }
+  star.style.setProperty("--distance", distance);
+
+  const init_distance_from_center = Math.sqrt(Math.pow(initX - 0.5 * window.innerWidth, 2) + Math.pow(initY - 0.5 * window.innerHeight, 2));
+  const time_to_leave = distance * (1 - init_distance_from_center / (trajectory_distance + init_distance_from_center)) / our_speed;
+  star.style.setProperty("--time-to-leave", time_to_leave);
+}
 
 
-/* BACKGROUND PAGE CONTENT */
+function generate(count, spawnFarAway) {
+  for (let i = 0; i < count; i++) {
+    const star = document.createElement("div");
+    randomizeStar(star, spawnFarAway);
+    star.classList.add("bkg-star");
 
-const background_page = document.querySelector('.background-page');
+    star.addEventListener("animationend", () => {
+      if (!star.classList.contains("slide")) {
+        generate(1, true);
+        star.remove();
+      }
+    });
+
+    stars_container.appendChild(star);
+  }
+}
+
+generate((window.innerWidth + window.innerHeight) / 8, false);
+document.body.style.overflowY = 'hidden';
+
+
+/* GO TO CONTENT */
+
 const background_hello = document.getElementById('background-hello');
+const asso_hello = document.getElementById("asso-hello");
+const background_page = document.getElementById('background-page');
+const background_page_sub = document.getElementById('background-page-sublayer');
 const page_content = document.getElementById("page-content");
 const menu = document.getElementById("menu-container");
 
-window.addEventListener('scroll', () => {
-    if (page_content.getBoundingClientRect().top < window.innerHeight) {
-      background_page.style.width = '101%';
-      background_page.style.height = '101%';
-      background_page.style.borderRadius = '0';
-      background_page.style.backgroundColor = 'var(--background-color-main)';
-    } else {
-      background_page.style.width = '0';
-      background_page.style.height = '0';
-      background_page.style.backgroundColor = 'var(--background-color-second)';
-      background_page.style.borderRadius = '50%';
-    }
+function goToContent() {
+  document.body.removeEventListener("mousewheel", goToContent, false);
+  document.body.removeEventListener("DOMMouseScroll", goToContent, false);
+  document.body.removeEventListener("touchmove", goToContent, false);
 
-    if (page_content.getBoundingClientRect().top <= 0.5 && page_content.getBoundingClientRect().top >= 0) {
-      setTimeout(() => {
-        asso_hello.remove();
-        background_hello.remove();
-        menu.style.opacity = 1;
-      }, 1000);
-    }
+  background_page.classList.add("show");
+  background_page_sub.classList.add("show");
+  page_content.classList.add("show");
 
-});
+  setTimeout(() => {
+    asso_hello.remove();
+    background_hello.remove();
+    menu.style.opacity = 1;
+    document.body.style.overflowY = 'scroll';
+  }, 500);
+
+}
+
+document.body.addEventListener("mousewheel", goToContent, false);
+document.body.addEventListener("DOMMouseScroll", goToContent, false);
+document.body.addEventListener("touchmove", goToContent, false);
