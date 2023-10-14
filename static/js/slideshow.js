ca998b8281d53be6283cec785d2ae703060589bf
titfora <titouan821@gmail.com>
2023-10-14 19:48:31 +0200

    slideshow scrolling done

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -6,28 +6,54 @@ document.querySelectorAll('.row-slidewshow-container').forEach(slideshow => {
 
     let cards_order = [];
 
-    function scrollLeft() {
+    let cur_timeout;
+
+    function scroll(left) {
 
         for (let i = 0; i < slideshow.children.length; i++) {
-            cards_order[i] -= 1;
+            let direction = 1;
+            if (left) direction = -1;
+
+            cards_order[i] += direction;
             if (cards_order[i] < - Math.ceil(slideshow.children.length / 2)) {
                 slideshow.children[i].style.opacity = 0;
                 cards_order[i] = slideshow.children.length - 1 - Math.ceil(slideshow.children.length / 2);
             } else {
-                if (cards_order[i] === slideshow.children.length - 1 - Math.ceil(slideshow.children.length / 2) - 1) {
+                if (cards_order[i] > slideshow.children.length - 1 - Math.ceil(slideshow.children.length / 2)) {
+                    slideshow.children[i].style.opacity = 0;
+                    cards_order[i] = - Math.ceil(slideshow.children.length / 2);
+                } else {
                     slideshow.children[i].style.opacity = 1;
                 }
             }
 
-            const transOff = cards_order[i] * (2 * slideshow.children[(i - cards_order[i] + slideshow.children.length) % slideshow.children.length].offsetWidth / (Math.abs(cards_order[i]) + 1));
+            const transOff = cards_order[i] * (2 * slideshow.children[(i - direction + slideshow.children.length) % slideshow.children.length].offsetWidth / (Math.abs(cards_order[i]) + 1));
             slideshow.children[i].style.transform = "translateX(" + transOff + "px) scale(" + scale_adjust / (Math.abs(cards_order[i]) + scale_adjust) + ")";
             slideshow.children[i].style.zIndex = -1 * Math.abs(cards_order[i]) + zIndex_offset;
             slideshow.children[i].style.filter = "blur(" + Math.abs(cards_order[i]) * 4 + "px)";
         }
     
-        setTimeout(() => {
-            scrollLeft(slideshow);
+        cur_timeout = setTimeout(() => {
+            scroll(left);
         }, show_time);
+    }
+
+    let touchMoveStartX = 0;
+    function onScroll(event) {
+        slideshow.removeEventListener("wheel", onScroll);
+        slideshow.removeEventListener("touchmove", onScroll, { passive: false });
+
+        const delta = event.deltaX || (touchMoveStartX - event.touches[0].pageX);
+
+        event.preventDefault();
+
+        clearTimeout(cur_timeout);
+        scroll((delta > 0));
+
+        setTimeout(() => {
+            slideshow.addEventListener("wheel", onScroll);
+            slideshow.addEventListener("touchmove", onScroll, { passive: false });
+        }, 200);
     }
 
     // add some cards if needed
@@ -44,6 +70,12 @@ document.querySelectorAll('.row-slidewshow-container').forEach(slideshow => {
         cards_order.push(i - Math.ceil(slideshow.children.length / 2));
     }
 
-    scrollLeft();
+    scroll(true);
+
+    slideshow.addEventListener("wheel", onScroll);
+    slideshow.addEventListener("touchstart", (event) => {
+        touchMoveStartX = event.touches[0].pageX;
+    }, { passive: true })
+    slideshow.addEventListener("touchmove", onScroll, { passive: false });
 
 });
