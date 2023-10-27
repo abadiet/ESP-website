f6ba2df8db2f7d42cd528d9f88e9bb56132aef75
titfora <titouan821@gmail.com>
2023-10-27 23:25:38 +0200

    clean

--- static/js/viewport.js
+++ static/js/viewport.js
@@ -1,5 +1,4 @@
 document.documentElement.style.setProperty('--viewport-height', `${window.innerHeight}px`);
 window.addEventListener('resize', () => {
     document.documentElement.style.setProperty('--viewport-height', `${window.innerHeight}px`);
-    document.body.height = window.innerHeight;
 });
